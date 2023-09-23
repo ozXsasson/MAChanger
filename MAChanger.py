720744f8b78274103e20a7f926cff07e8f04e2e1
Oz Sasson <145783741+ozXsasson@users.noreply.github.com>
2023-09-23 18:48:35 -0400

    Added random MAC address feature

--- MAChanger.py
+++ MAChanger.py
@@ -3,24 +3,26 @@
 import subprocess
 import optparse
 import re
+import random
 
 def getArgu():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    parser.add_option("-r", "--random", dest="random", help="Generate random MAC address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("--- Please specify an interface, use --help for more info")
-    elif not options.new_mac:
-        parser.error("--- Please specify a new mac, use --help for more info")
     return options
-    
+
+
 def changeMAC(interface, new_mac):
     print("*** Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
-    
+
+
 def getMAC(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
@@ -30,15 +32,23 @@ def getMAC(interface):
         print("--- Could not read MAC address")
 
 
+def generateMAC():
+    digits = [random.choice('0123456789ABCDEF') for _ in range(11)]
+    second_digit = random.choice('02468ACE')
+    digits.insert(1, second_digit)
+    mac_address = ':'.join(''.join(digits[i:i + 2]) for i in range(0, 12, 2))
+    return mac_address
+
 options = getArgu()
-
+newMAC = generateMAC()
 myMAC = getMAC(options.interface)
-print("*** Current MAC = " + str(myMAC))
+print("*** Current MAC = " + myMAC)
 
-changeMAC(options.interface, options.new_mac)
 
-myMAC = getMAC(options.interface)
-if myMAC == options.new_mac:
-    print("*** MAC address was successfully changed to " + myMAC)
+if options.new_mac:
+    changeMAC(options.interface, options.new_mac)
 else:
-    print("--- MAC adddress did not get changed.")
+    changeMAC(options.interface, newMAC)
+
+myMAC = getMAC(options.interface)
+print("*** MAC address was successfully changed to " + myMAC)
