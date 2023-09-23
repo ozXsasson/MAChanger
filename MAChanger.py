c2b5b11d593178f8b97be9102f64e142e614322d
Oz Sasson <145783741+ozXsasson@users.noreply.github.com>
2023-09-23 17:14:18 -0400

    Update MAChanger.py

--- MAChanger.py
+++ MAChanger.py
@@ -14,11 +14,13 @@ def getArgu():
     elif not options.new_mac:
         parser.error("--- Please specify a new mac, use --help for more info")
     return options
+    
 def changeMAC(interface, new_mac):
     print("*** Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
+    
 def getMAC(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
